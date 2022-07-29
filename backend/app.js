76440ffccdadc3c92353eec6450a630b8e652d0a
Elybourland <JEBourland@yahoo.com>
2022-07-28 17:28:00 -0700

    storing data in database

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const Post = require('./models/post');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://elybourland:Up5RtKMsnXzsXJjd@cluster0.mgssfjc.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://elybourland:Up5RtKMsnXzsXJjd@cluster0.mgssfjc.mongodb.net/node-angular?retryWrites=true&w=majority')
 .then(() => {
   console.log('Connected to database!');
 })
@@ -36,7 +36,7 @@ app.post('/api/posts', (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: 'Post added successfully'
   });
