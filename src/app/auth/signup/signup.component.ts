1a7142b7fa95b6cde66f72cb5dcf34f0eb84d537
Elybourland <JEBourland@yahoo.com>
2022-08-15 14:48:58 -0700

    sending token to frontend

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -14,8 +14,7 @@ export class SignupComponent {
   onSignup(form: NgForm) {
     if (form.invalid) {
       return;
-    } else {
+    }
       this.authService.createUser(form.value.email, form.value.password);
     }
   }
-}
