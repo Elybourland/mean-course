aa929cb43424afe3597a82105eb247afabd0ced1
Elybourland <JEBourland@yahoo.com>
2022-08-20 12:39:26 -0700

    using the user ID on the frontend

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -46,7 +46,7 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return this.http.get<{ _id: string, title: string, content: string, imagePath: string }>(
+    return this.http.get<{ _id: string, title: string, content: string, imagePath: string, creator: string }>(
       "http://localhost:3000/api/posts/" + id
     );
   }
@@ -79,7 +79,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
