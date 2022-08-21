0c928341303aeb7872182c182203623ae9364556
Elybourland <JEBourland@yahoo.com>
2022-08-21 16:31:56 -0700

    testing different places to handle errors

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -32,11 +32,12 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post("http://localhost:3000/api/user/signup", authData)
-      .subscribe(response => {
-        console.log(response);
-      });
-    this.router.navigate(['/login']);
+     return this.http.post("http://localhost:3000/api/user/signup", authData)
+      .subscribe(() => {
+      this.router.navigate(['/']);
+    }, error => {
+      this.authStatusListener.next(false);
+    });
   }
 
   login(email: string, password: string) {
@@ -57,6 +58,8 @@ export class AuthService {
           this.saveAuthData(token, expirationDate, this.userId);
           this.router.navigate(['/']);
         }
+      }, error => {
+        this.authStatusListener.next(false);
       });
   }
 
